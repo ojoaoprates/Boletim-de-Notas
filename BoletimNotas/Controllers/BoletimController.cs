6f4fc4da95ed87283094aedc44392b65431313d7
João Vitor Prates <jvprates63@gmail.com>
2026-06-10 02:36:20 +0000

    BoletimNotas att

--- BoletimNotas/Controllers/BoletimController.cs
+++ BoletimNotas/Controllers/BoletimController.cs
@@ -10,10 +10,10 @@ namespace BoletimNotas.Controllers
         {
             var boletim = new List<BoletimItem>
             {
-                new BoletimItem { Id = 1, Materia = "Desenvolvimento para internet", Nota = 8.5m, Professor = "Prof. Ana", Observacao = "Bom desempenho" },
-                new BoletimItem { Id = 2, Materia = "Arquitetura de computadores", Nota = 7.0m, Professor = "Prof. Bruno", Observacao = "Precisa revisar microarquitetura" },
-                new BoletimItem { Id = 3, Materia = "Programação Front-End", Nota = 9.2m, Professor = "Prof. Carla", Observacao = "Excelente em CSS e JS" },
-                new BoletimItem { Id = 4, Materia = "IoT", Nota = 6.8m, Professor = "Prof. Diego", Observacao = "Melhorar integração de sensores" }
+                new BoletimItem { Id = 1, Materia = "Desenvolvimento para internet", Nota = 8.5m, Professor = "Prof. José Romualdo" },
+                new BoletimItem { Id = 2, Materia = "Arquitetura de computadores", Nota = 7.0m, Professor = "Prof. Helio" },
+                new BoletimItem { Id = 3, Materia = "Programação Front-End", Nota = 9.2m, Professor = "Prof. Herbert" },
+                new BoletimItem { Id = 4, Materia = "IoT", Nota = 6.8m, Professor = "Prof. Norberto" }
             };
 
             ViewBag.Boletim = boletim;
